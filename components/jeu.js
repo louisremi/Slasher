82ac405d36d0b6c8bb683d55dcac8efbc4dd66f4
julien DEVILLE <julien.deville@gmail.com>
2012-11-24 19:58:05 +0100

    musique

---
+++ components/jeu.js
@@ -0,0 +1,59 @@
+Crafty.c("Jeu",{
+			menu: null,
+			bouton: null,
+			teens: null,
+			slasher: null,
+			musique: null,
+			Pause:function(){
+				if( this.menu )
+			  		this.menu.css({ "display": "block" });
+				if( this.bouton )
+			  		this.bouton.css({ "display": "block" });
+				if( this.teens )
+			  		for( i in this.teens)
+			  			this.teens[i].pause();
+				if( this.slasher )
+			  		this.slasher.pause();
+				if( this.musique )
+			  		this.musique.menu();
+				if( this.animations )
+			  		this.animations.pause();
+			  return this;
+			},
+			Resume:function(){
+				if( this.menu )
+			  		this.menu.css({ "display": "none" });
+				if( this.bouton )
+			  		this.bouton.css({ "display": "none" });
+				if( this.teens )
+			  		for( i in this.teens)
+			  			this.teens[i].resume();
+				if( this.slasher )
+			  		this.slasher.resume();
+				if( this.musique )
+			  		this.musique.jeu();
+				if( this.animations )
+			  		this.animations.resume();
+			  return this;
+			},
+			init:function(){
+			  var _this = this;
+			  this.requires("2D");
+			  this.teens = [];
+			  this.menu = Crafty.e("2D, DOM, Text, Color").attr({ w: 1024, h: 600, x: 0, y: 0, z: 990 })
+			  					 .color("#ddd")
+				                 .text("Pause")
+				                 .css({ "text-align": "center"
+				                 	, "padding-top": "250px"
+				                 	, "font-size": "64pt"
+				                 	,"display": "none" });
+			  this.bouton = Crafty.e("2D, DOM, Text, Color, Mouse").attr({ w: 200, h: 40, x: 412, y: 500, z: 991 })
+			  					 .color("#4F4")
+				                 .text("<p>Revenir au jeu</p>")
+				                 .css({ "text-align": "center"
+				                 	,"display": "none"})
+				                 .bind("Click", function() {
+				                 	_this.Resume();
+				                 } );
+			}
+		});
---
+++ components/musique.js
@@ -0,0 +1,68 @@
+Crafty.c("Musique",{
+			ingame: null,
+			titre: null,
+			peurs: null,
+			morts: null,
+			gameover: null,
+			gamewin: null,
+			slasher: null,
+			playing: null,
+			menu:function( precedent ){
+				for( id in this.playing ) {
+					Crafty.audio.togglePause(this.playing[id]);
+				/*if( precedent){
+					for( id in this.playing ) {
+						Crafty.audio.stop(precedent);
+					}*/
+				}
+				this.playing = [];
+				
+				//var musique = this.titre[Math.floor((Math.random()*this.titre.length))]; 
+				Crafty.audio.play( "ingame4");
+				this.playing[this.playing.length] = "ingame4";
+				Crafty.audio.play( "titre");
+				this.playing[this.playing.length] = "titre";
+				
+				return this;
+			},
+			jeu:function( precedent ){
+				for( id in this.playing ) 
+					Crafty.audio.togglePause(this.playing[id]);
+				this.playing = [];
+					
+				var musique = this.ingame[Math.floor((Math.random()*this.ingame.length))]; 
+				this.playing[this.playing.length] = musique;
+				Crafty.audio.play( musique);
+				
+			  return this;
+			},
+			peur:function(){
+				
+			  return this;
+			},
+			mort:function(){
+				
+			  return this;
+			},
+			init:function(){
+			  var _this = this;
+			  this.requires("Audio");
+			  this.ingame = [];
+			  this.peurs = [];
+			  this.morts = [];
+			  this.gameover = [];
+			  this.gamewin = [];
+			  this.slasher = [];
+			  this.titre = [];
+			  this.playing = [];
+			  
+			  this.load( "ingame", this.ingame, ["assets/ingame.wav","assets/ingame.ogg"] );
+			  this.load( "ingame4", this.titre, ["assets/in game4.wav","assets/in game4.ogg"] );
+			  this.load( "titre", this.titre, ["assets/Titre.wav","assets/Titre.ogg"] );
+
+			},
+			load: function( name, cat, url ) {
+				cat[cat.length] = name;
+				Crafty.audio.add( name, url );	
+			}
+		});
