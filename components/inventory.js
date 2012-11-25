684e3acb19bb3f8f300f49e513a5c5211df7f449
louisremi <lrbabe@gmail.com>
2012-11-25 10:49:35 +0100

    Initial commit of the wolf trap

--- components/inventory.js
+++ components/inventory.js
@@ -28,25 +28,7 @@ Crafty.c("Inventory", {
 					.saveOrigin();
 			}
 			offset++;
-		});/*
-
-		Crafty.Traps.forEach(function( name ) {
-
 		});
-
-		$.each(traps,function(key,value) {
-
-			var trap = Crafty.e('2D, DOM, Trap, Mouse, '+key+', Selectable')
-					.attr({x:16,y:offset*self.basicOffset,z:self._z+1})
-					.postNumber(value.value)
-					.setEffect(value.callback)
-					.bind('Click',function(e){
-						this.selectEntity(function() {});
-					});
-
-			self.inventory.push(trap);
-			offset++;
-		});*/
 
 		return this;
 	}
@@ -54,17 +36,17 @@ Crafty.c("Inventory", {
 
 Crafty.sprite(32,"assets/sprites/zob.png",{
 	PikesSprite:[0,0],
-	HoleSprite:[0,1]/*,
+	WolfTrapSprite:[0,1]/*,
+	HoleSprite:[0,1],
 	SmokeSprite:[2,0],
-	WolfTrapSprite:[3,0],
 	AcidSprite:[4,0]*/
 });
 
 Crafty.Inventory = {
-	Pikes: 3/*,
+	Pikes: 3,
+	WolfTrap: 3/*,
 	Hole: 3,
 	Smoke: 3,
-	WolfTrap: 3,
 	Acid: 3*/
 };
 
