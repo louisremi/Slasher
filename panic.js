76bbd4a7065b2ae4e51612d92bed16acb9bec2e8
FranzP <franzp@Fearne-Cotton.(none)>
2012-11-25 15:18:48 +0100

    mort

--- panic.js
+++ panic.js
@@ -10,11 +10,15 @@
                 .image("assets/sprites/uiPanic.png");
         this.screen.tween({alpha:1.0},200).tween({alpha:0.0},200).tween({x: 0},50).tween({x: 300},50);
 
-
         Jeu.musique.panic();
 
         Crafty.sprite(104, "assets/sprites/teenagers.png", {
         	SlasherSprite:[1,5],
+        });
+
+        Crafty.sprite(64, "assets/sprites/texture.png", {
+            upWindowBreak:[4,4],
+            downWindowBreak:[4,5],
         });
 
         Crafty.PathFinder.enterPanicMode();
@@ -25,11 +29,11 @@
         	position.y = door._y;
         })
 
-        Crafty.e('2D,DOM,Slasher, SlasherSprite,Keyboard,Collision,WiredHitBox')
+        Crafty.e('2D,DOM,Slasher, SlasherSprite,Keyboard,Collision')
         	.attr(position)
-        	.slasher(1)
+        	.slasher(4)
         	.collision(
-        		new Crafty.polygon([0+16,64+32],[64+16,64+32],[64+16,0+32],[0+16,0+32]))
+        		new Crafty.polygon([1+16,63+32],[63+16,63+32],[63+16,1+32],[1+16,1+32]))
         	.bind('Moved', function(from) {
         		var collision = this.hit('blocked')
 			    if(collision){
