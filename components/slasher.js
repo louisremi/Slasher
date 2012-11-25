12327d9b6d80dee3d3f72ef6f14a42c4c1f0deaf
FranzP <franzp@Fearne-Cotton.(none)>
2012-11-25 13:41:22 +0100

    ajout slasher

--- components/slasher.js
+++ components/slasher.js
@@ -1,6 +1,15 @@
 (function(Crafty) {
 
 	Crafty.c('Slasher',{
+		init:function() {
+			this.requires('Multiway');
+		},
+
+		slasher: function(speed) {
+	        this.multiway(speed, {W: -90, S: 90, D: 0, A: 180})
+	        return this;
+    	},
+    }
 
 	});
 
